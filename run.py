bbac4d4c65d43f77e1eb0fb903e445cef21f55d3
kevin <68902447+kevinJ045@users.noreply.github.com>
2022-10-21 23:03:37 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+from Builder import *
+
+
+# java_builder.start();
+
+# java_builder.jar("Source/bin/com/filecon/MainActivity","Source/bin")
+
+java_builder.run("./Source/src/com/filecon MainActivity.java")
